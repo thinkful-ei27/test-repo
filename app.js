45801ad23cf60b8ba8099f5e1dd9259c47006151
dhcohn <davidhcohn@gmail.com>
2018-12-03 15:06:28 -0500

    says year of birth

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let myTeam = "Peter David Jason";
 console.log(myTeam);
 function createGreeting(name, age) {
-    return `my name is ${name} and I am ${age}`;
+    const yearOfBirth = 2018 - age;
+    return `my name is ${name} and I am ${age}, I was born in ${yearOfBirth}`
 }
