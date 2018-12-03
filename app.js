a624472f9a0b0ae70f2a3a6a849d5932297de6ff
publikwerker <hello@publikwerks.com>
2018-12-03 12:11:48 -0800

    added function to calculate the year of birth

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let myTeam = "Peter David Jason";
 console.log(myTeam);
 function createGreeting(name, age) {
-    const yearOfBirth = 2018 - age;
-    return `my name is ${name} and I am ${age}, I was born in ${yearOfBirth}`
+    const yob = getYearofBirth(age);
+    return `my name is ${name} and I am ${age}, I was born in ${yob}`
 }
+ function getYearofBirth(age) {
+    const yearOfBirth = 2018 - age;
+    return yearOfBirth;
+ }
