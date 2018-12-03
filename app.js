84b447659bb43d273558f522f1891551babc2f5a
publikwerker <hello@publikwerks.com>
2018-12-03 12:25:59 -0800

    added try/catch for age

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ function createGreeting(name, age) {
     return `my name is ${name} and I am ${age}, I was born in ${yob}`
 }
  function getYearofBirth(age) {
+     if (age < 0 ) {
+         throw new Error("Age can not be negative");
+     }
+     try {
+         createGreeting('bob', -1);
+     }
+     catch(e) {
+         console.error(e.message);
+     }
     const yearOfBirth = 2018 - age;
     return yearOfBirth;
  }
+ getYearofBirth();
+ 
