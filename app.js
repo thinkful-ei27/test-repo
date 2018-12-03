b0ad513ddce57851108bae894739d4333b77cc3e
Peter <peterdramos02@gmail.com>
2018-12-03 15:50:08 -0500

    fool around with try/catch and throwing errors

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
-let myTeam = "Peter David Jason";
+let myTeam = 'Peter David Jason';
 console.log(myTeam);
 function createGreeting(name, age) {
-    const yob = getYearofBirth(age);
-    return `my name is ${name} and I am ${age}, I was born in ${yob}`
+    checkIfParameterExists(name, age);
+    checkIfParameterIsRightType(name, age);
+      const yob = getYearofBirth(age);
+      return `my name is ${name} and I am ${age}, I was born in ${yob}`
 }
+
+function checkIfParameterExists (name, age) {
+  if (!name || !age) {
+    throw new Error('Please provide name and age');
+  }
+}
+
+function checkIfParameterIsRightType(name, age) {
+  if (typeof name !== 'string' || typeof age !== 'number') {
+    throw new TypeError('name must be a string and age must be a number')
+  }
+}
+
  function getYearofBirth(age) {
      if (age < 0 ) {
-         throw new Error("Age can not be negative");
+         throw new Error('Age can not be negative');
      }
-     try {
-         createGreeting('bob', -1);
-     }
-     catch(e) {
-         console.error(e.message);
-     }
+    //  try {
+    //      createGreeting('bob', -1);
+    //  }
+    //  catch(e) {
+    //      console.error(e.message);
+    //  }
     const yearOfBirth = 2018 - age;
     return yearOfBirth;
  }
- getYearofBirth();
+//  getYearofBirth();
+ createGreeting("Peter", 49);
  
